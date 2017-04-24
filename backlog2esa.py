eecc95dc0f03e41e7ef53851ebcc0f57c48b8c26
hidetomo.watanabe <hidetomo.watanabe@soinn.com>
2017-04-24 05:04:21 +0000

    rename

--- backlog2esa.py
+++ backlog2esa.py
@@ -2,10 +2,14 @@ import sys
 from webapi import WebApi
 
 if __name__ == '__main__':
-    src_target, src_base_url, src_api_key = sys.argv[1:4]
-    src_project_key = sys.argv[4]
-    dst_target, dst_base_url, dst_api_key = sys.argv[5:8]
-    dst_path = sys.argv[8]
+    src_target = 'backlog'
+    src_base_url = 'https://soinn.backlog.jp/api/v2/wikis'
+    src_api_key = sys.argv[1]
+    src_project_key = sys.argv[2]
+    dst_target = 'esa.io'
+    dst_base_url = 'https://api.esa.io/v1/teams/soinn/posts'
+    dst_api_key = sys.argv[3]
+    dst_path = sys.argv[4]
 
     src_obj = WebApi(src_target, src_base_url, src_api_key)
     dst_obj = WebApi(dst_target, dst_base_url, dst_api_key)
