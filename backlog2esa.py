bc82435db912b9d269d6a80b5f4e926d56c338b4
hidetomo.watanabe <hidetomo.watanabe@soinn.com>
2017-04-24 05:23:10 +0000

    fix team

--- backlog2esa.py
+++ backlog2esa.py
@@ -2,14 +2,15 @@ import sys
 from webapi import WebApi
 
 if __name__ == '__main__':
+    team = sys.argv[1]
     src_target = 'backlog'
-    src_base_url = 'https://XXX.backlog.jp/api/v2/wikis'
-    src_api_key = sys.argv[1]
-    src_project_key = sys.argv[2]
+    src_base_url = 'https://%s.backlog.jp/api/v2/wikis' % team
+    src_api_key = sys.argv[2]
+    src_project_key = sys.argv[3]
     dst_target = 'esa.io'
-    dst_base_url = 'https://api.esa.io/v1/teams/XXX/posts'
-    dst_api_key = sys.argv[3]
-    dst_path = sys.argv[4]
+    dst_base_url = 'https://api.esa.io/v1/teams/%s/posts' team
+    dst_api_key = sys.argv[4]
+    dst_path = sys.argv[5]
 
     src_obj = WebApi(src_target, src_base_url, src_api_key)
     dst_obj = WebApi(dst_target, dst_base_url, dst_api_key)
